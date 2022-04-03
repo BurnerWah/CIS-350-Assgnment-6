59639e4fd179970ec7291a9c3c92531d7dd3566e
Jaden Pleasants <jadenpleasants@fastmail.com>
2022-04-03 16:39:15 -0500

    Some polymorphism stuff

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,6 +10,16 @@ using UnityEngine;
 public class Weapon : MonoBehaviour {
     public int damageBonus;
 
+    public Enemy enemyHoldingWeapon;
+
+    private void Awake() {
+        enemyHoldingWeapon = gameObject.GetComponent<Enemy>();
+    }
+
+    protected void EnemyEatsWeapon(Enemy enemy) {
+        Debug.Log("Enemy Eats Weapon");
+    }
+
     public void Recharge() {
         Debug.Log("Recharging Weapon!");
     }
